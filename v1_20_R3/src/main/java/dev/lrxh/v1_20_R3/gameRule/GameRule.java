dea6867e4beaaf6e129b10c05c26c8758e2a8d04
devlrxxh <seabearvalorant@gmail.com>
2024-05-17 06:54:10 +0200

    Fixed 1.20 gamrule

--- v1_20_R3/src/main/java/dev/lrxh/v1_20_R3/gameRule/GameRule.java
+++ v1_20_R3/src/main/java/dev/lrxh/v1_20_R3/gameRule/GameRule.java
@@ -7,7 +7,7 @@ public class GameRule implements IGameRule {
     @Override
     public void setGameRule(World world, dev.lrxh.gameRule.GameRule gameRule, boolean value) {
         if (gameRule == null) return;
-        org.bukkit.GameRule rule = org.bukkit.GameRule.getByName(gameRule.getLegacy());
+        org.bukkit.GameRule rule = org.bukkit.GameRule.getByName(gameRule.toString());
         if (rule != null) {
             world.setGameRule(rule, value);
         }
