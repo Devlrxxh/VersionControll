c0fed717ef21aef4c9dfa1b499635d0bf9bc3da5
devlrxxh <seabearvalorant@gmail.com>
2024-05-20 00:49:45 +0200

    Removed debug message

--- Build/src/main/java/dev/lrxh/versioncontroll/VersionControll.java
+++ Build/src/main/java/dev/lrxh/versioncontroll/VersionControll.java
@@ -13,7 +13,6 @@ public class VersionControll {
     public VersionControll(JavaPlugin plugin) {
         this.plugin = plugin;
         this.handler = findVersion(plugin.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3]);
-        plugin.getLogger().info(plugin.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3]);
     }
 
     private VersionHandler findVersion(String version) {
