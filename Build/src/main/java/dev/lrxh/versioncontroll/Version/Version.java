d3031159c8c716199e901364c1609c8a579aa0b9
devlrxxh <seabearvalorant@gmail.com>
2024-05-19 10:28:06 +0200

    Added 1.18

--- Build/src/main/java/dev/lrxh/versioncontroll/Version/Version.java
+++ Build/src/main/java/dev/lrxh/versioncontroll/Version/Version.java
@@ -2,17 +2,22 @@ package dev.lrxh.versioncontroll.Version;
 
 import dev.lrxh.v1_20_R3.v1_20_R3;
 import dev.lrxh.v1_19_R3.v1_19_R3;
+import dev.lrxh.v1_18_R3.v1_18_R3;
 import dev.lrxh.v1_17_R3.v1_17_R3;
 import lombok.Getter;
 
 @Getter
 public enum Version {
+    v1_20_R4(new Attributes(true, new v1_20_R3())),
     v1_20_R3(new Attributes(true, new v1_20_R3())),
     v1_20_R2(new Attributes(true, new v1_20_R3())),
     v1_20_R1(new Attributes(true, new v1_20_R3())),
+    v1_19_R4(new Attributes(true, new v1_19_R3())),
     v1_19_R3(new Attributes(true, new v1_19_R3())),
     v1_19_R2(new Attributes(true, new v1_19_R3())),
     v1_19_R1(new Attributes(true, new v1_19_R3())),
+    v1_18_R2(new Attributes(true, new v1_18_R3())),
+    v1_18_R1(new Attributes(true, new v1_18_R3())),
     v1_17_R3(new Attributes(true, new v1_17_R3())),
     v1_17_R2(new Attributes(true, new v1_17_R3())),
     v1_17_R1(new Attributes(true, new v1_17_R3()));
---
+++ v1_18_R3/src/main/java/dev/lrxh/v1_18_R3/v1_18_R3.java
@@ -0,0 +1,17 @@
+package dev.lrxh.v1_18_R3;
+
+import dev.lrxh.VersionHandler;
+import dev.lrxh.v1_18_R3.gameRule.GameRule;
+import dev.lrxh.v1_18_R3.sounds.Sound;
+import dev.lrxh.v1_18_R3.utils.ColorUtils;
+import dev.lrxh.v1_18_R3.config.Config;
+import dev.lrxh.v1_18_R3.utils.ChatComponent;
+import dev.lrxh.v1_18_R3.utils.PlayerUtils;
+import dev.lrxh.v1_18_R3.utils.chunk.ChunkUtilsUtil;
+
+public class v1_18_R3 extends VersionHandler {
+    public v1_18_R3() {
+        super(new PlayerUtils(),
+                new Config(), new GameRule(), new ColorUtils(), new ChatComponent(), new Sound(), new ChunkUtilsUtil());
+    }
+}
