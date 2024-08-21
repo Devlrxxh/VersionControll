d267e2ef572e1e4a6a73332b2b310c2287e40e28
Devlrxxh <seabearvalorant@gmail.com>
2024-08-21 15:35:23 +0300

    Added 1.20.5 and 1.20.6 support

--- Build/src/main/java/dev/lrxh/versioncontroll/Version/Version.java
+++ Build/src/main/java/dev/lrxh/versioncontroll/Version/Version.java
@@ -12,6 +12,8 @@ import javax.annotation.Nullable;
 
 @Getter
 public enum Version {
+    v1_20_R6(),
+    v1_20_R5(),
     v1_20_R4(),
     v1_20_R3(),
     v1_20_R2(),
@@ -45,7 +47,7 @@ public enum Version {
             case v1_19_R1, v1_19_R2, v1_19_R3, v1_19_R4 -> {
                 return new v1_19_R3();
             }
-            case v1_20_R3, v1_20_R1, v1_20_R2, v1_20_R4 -> {
+            case v1_20_R3, v1_20_R1, v1_20_R2, v1_20_R4, v1_20_R5, v1_20_R6 -> {
                 return new v1_20_R3();
             }
         }
