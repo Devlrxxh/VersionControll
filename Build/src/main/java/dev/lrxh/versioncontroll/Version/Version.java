09332509357c223715c56722cc94dfbdd42ec6a2
Devlrxxh <seabearvalorant@gmail.com>
2024-08-21 11:28:51 +0300

    Not making a new instance of each version on load

--- Build/src/main/java/dev/lrxh/versioncontroll/Version/Version.java
+++ Build/src/main/java/dev/lrxh/versioncontroll/Version/Version.java
@@ -8,29 +8,47 @@ import dev.lrxh.v1_19_R3.v1_19_R3;
 import dev.lrxh.v1_20_R3.v1_20_R3;
 import lombok.Getter;
 
+import javax.annotation.Nullable;
+
 @Getter
 public enum Version {
-    v1_20_R4(new v1_20_R3()),
-    v1_20_R3(new v1_20_R3()),
-    v1_20_R2(new v1_20_R3()),
-    v1_20_R1(new v1_20_R3()),
-    v1_19_R4(new v1_19_R3()),
-    v1_19_R3(new v1_19_R3()),
-    v1_19_R2(new v1_19_R3()),
-    v1_19_R1(new v1_19_R3()),
-    v1_18_R2(new v1_18_R3()),
-    v1_18_R1(new v1_18_R3()),
-    v1_17_R2(new v1_17_R3()),
-    v1_17_R1(new v1_17_R3()),
-    v1_16_R5(new v1_16_R3()),
-    v1_16_R4(new v1_16_R3()),
-    v1_16_R3(new v1_16_R3()),
-    v1_16_R2(new v1_16_R3()),
-    v1_16_R1(new v1_16_R3());
+    v1_20_R4(),
+    v1_20_R3(),
+    v1_20_R2(),
+    v1_20_R1(),
+    v1_19_R4(),
+    v1_19_R3(),
+    v1_19_R2(),
+    v1_19_R1(),
+    v1_18_R2(),
+    v1_18_R1(),
+    v1_17_R2(),
+    v1_17_R1(),
+    v1_16_R5(),
+    v1_16_R4(),
+    v1_16_R3(),
+    v1_16_R2(),
+    v1_16_R1();
 
-    private final VersionHandler versionHandler;
-
-    Version(VersionHandler versionHandler) {
-        this.versionHandler = versionHandler;
+    @Nullable
+    public VersionHandler getVersionHandler(){
+        switch (this) {
+            case v1_16_R1, v1_16_R2, v1_16_R3, v1_16_R4 , v1_16_R5-> {
+                return new v1_16_R3();
+            }
+            case v1_17_R1, v1_17_R2 -> {
+                return new v1_17_R3();
+            }
+            case v1_18_R1, v1_18_R2 -> {
+                return new v1_18_R3();
+            }
+            case v1_19_R1, v1_19_R2, v1_19_R3, v1_19_R4 -> {
+                return new v1_19_R3();
+            }
+            case v1_20_R3, v1_20_R1, v1_20_R2, v1_20_R4 -> {
+                return new v1_20_R3();
+            }
+        }
+        return null;
     }
 }
