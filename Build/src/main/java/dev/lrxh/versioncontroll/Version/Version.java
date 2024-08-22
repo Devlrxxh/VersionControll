e67b81f1cb42efdda9daee648031a6db31e28da3
Devlrxxh <seabearvalorant@gmail.com>
2024-08-22 18:37:39 +0300

    Added method to check if version higher than

--- Build/src/main/java/dev/lrxh/versioncontroll/Version/Version.java
+++ Build/src/main/java/dev/lrxh/versioncontroll/Version/Version.java
@@ -13,26 +13,28 @@ import javax.annotation.Nullable;
 
 @Getter
 public enum Version {
-    v1_21_R1(),
-    v1_20_R6(),
-    v1_20_R5(),
-    v1_20_R4(),
-    v1_20_R3(),
-    v1_20_R2(),
-    v1_20_R1(),
-    v1_19_R4(),
-    v1_19_R3(),
-    v1_19_R2(),
-    v1_19_R1(),
-    v1_18_R2(),
-    v1_18_R1(),
-    v1_17_R2(),
-    v1_17_R1(),
-    v1_16_R5(),
-    v1_16_R4(),
-    v1_16_R3(),
-    v1_16_R2(),
-    v1_16_R1();
+    v1_21_R1(20),
+    v1_20_R6(19),
+    v1_20_R5(18),
+    v1_20_R4(17),
+    v1_20_R3(16),
+    v1_20_R2(15),
+    v1_20_R1(14),
+    v1_19_R4(13),
+    v1_19_R3(12),
+    v1_19_R2(11),
+    v1_19_R1(10),
+    v1_18_R2(9),
+    v1_18_R1(8),
+    v1_17_R2(7),
+    v1_17_R1(6),
+    v1_16_R5(5),
+    v1_16_R4(4),
+    v1_16_R3(3),
+    v1_16_R2(2),
+    v1_16_R1(1);
+
+    private final int number;
 
     @Nullable
     public VersionHandler getVersionHandler(){
@@ -57,5 +59,13 @@ public enum Version {
             }
         }
         return null;
+    }
+
+    public boolean isHigherThan(Version version) {
+        return number >= version.number;
+    }
+
+    Version(int number) {
+        this.number = number;
     }
 }
